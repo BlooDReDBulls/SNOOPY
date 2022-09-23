a5d41363eba608847ef40aca7108e5ba743e46da
TheGenyus <lucas.lalida@gmail.com>
2022-09-23 11:53:59 +0200

    Creation matrix

--- Version_1/src/Game.java
+++ Version_1/src/Game.java
@@ -1,2 +1,21 @@
+import java.util.Timer;
+
 public class Game {
+
+    int[][] matrix = new int[10][20];
+
+    public void Game()
+    {
+        for(int i = 0 ; i < 10 ; i++)
+        {
+            for(int j = 0 ; j < 20 ; j++)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    static void main(String[] args)
+    {
+
+    }
 }
