61b734dd2cf168c5800c9b3217205982ef5ca4a5
TheGenyus <lucas.lalida@gmail.com>
2022-10-09 20:10:30 +0200

    Creation of the timer that controls the limit of time the player has to finish a level and the creation of the timer that controls the refresh rate

--- Version_1/src/Game.java
+++ Version_1/src/Game.java
@@ -1,32 +1,48 @@
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Game {
 
-    public Timer timer = new Timer();
-    public TimerTask task = new TimerTask() {
+    int[][] map = new int[10][20];
+    public Timer timerController = new Timer();
+
+    public Game() {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 20; j++) {
+                map[i][j] = 0;
+            }
+        }
+    }
+    public TimerTask timer = new TimerTask() {
         @Override
         public void run() {
-            System.out.println("test");
+            System.out.println("Fin");
         }
     };
-    int[][] matrix = new int[10][20];
 
-    public void Game()
+    public TimerTask refresh = new TimerTask() {
+        @Override
+        public void run() {
+            displayMap();
+        }
+    };
+
+    public void displayMap()
     {
         for(int i = 0 ; i < 10 ; i++)
         {
             for(int j = 0 ; j < 20 ; j++)
             {
-                matrix[i][j] = 0;
+                System.out.print(map[i][j] + " ");
             }
+            System.out.println();
         }
     }
+
     public static void main(String[] args)
     {
         Game game = new Game();
-        game.timer.schedule(game.task, 1000, 500);
+        game.timerController.schedule(game.timer, 60000);
+        game.timerController.schedule(game.refresh, 200, 200);
     }
 }
