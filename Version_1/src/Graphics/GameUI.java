945e78c003f7ff6c3a4ebbb8806c7a6910815b29
LucienCH <chauvin.lucien.perso@gmail.com>
2022-10-23 21:57:44 +0200

    Add Animations Engine and Animations for snoopy bird and slider

--- Version_1/src/Graphics/GameUI.java
+++ Version_1/src/Graphics/GameUI.java
@@ -10,11 +10,11 @@ public class GameUI extends JPanel implements Observateur {
 
     private final Game game;
 
-    private Animations animations;
-    private TexturesImages texturesImages;
-    private TextureSprite textureSprite;
+    private final Animations animations;
+    private final TexturesImages texturesImages;
+    private final TextureSprite textureSprite;
 
-    private JFrame jFrame;
+    private final JFrame jFrame;
 
     public GameUI(Game game){
         animations = new Animations(60);
@@ -26,8 +26,6 @@ public class GameUI extends JPanel implements Observateur {
         jFrame = new JFrame("Snoopy le jeu");
         this.setPreferredSize(new Dimension(640,320));
         this.game = game;
-
-
         setupJFrame();
     }
 
@@ -55,31 +53,30 @@ public class GameUI extends JPanel implements Observateur {
         for (int i = 0; i < 10.; i++) {
             for (int j = 0; j < 20; j++) {
 
-                switch (game.getMap()[i][j]){
-                    case 3:
+                switch (game.getMap()[i][j]) {
+                    case 3 -> {
                         g2.drawImage(texturesImages.getImageFromTAB(0), (32 * j), (32 * i), this);
                         g2.drawImage(texturesImages.getImageFromMap(game.getMap()[i][j]), (32 * j), (32 * i), this);
-                        break;
-                    case 6:
+                    }
+                    case 6 -> {
                         g2.drawImage(texturesImages.getImageFromTAB(0), (32 * j), (32 * i), this);
-                        g2.drawImage(textureSprite.getSprite(6,0,animations.getCycleProgress(),4), (32 * j), (32 * i), this);
-                        break;
-                    case 7:
+                        g2.drawImage(textureSprite.getSprite(6, 0, animations.getCycleProgress(), 4), (32 * j), (32 * i), this);
+                    }
+                    case 7 -> {
                         g2.drawImage(texturesImages.getImageFromTAB(0), (32 * j), (32 * i), this);
                         g2.drawImage(texturesImages.getImageFromMap(game.getMap()[i][j]), (32 * j), (32 * i), this);
-                        break;
-                    case 8:
+                    }
+                    case 8 -> {
                         g2.drawImage(texturesImages.getImageFromTAB(0), (32 * j), (32 * i), this);
-                        g2.drawImage(textureSprite.getSprite(8,3,animations.getCycleProgress(),4), (32 * j), (32 * i), this);
-                        break;
-                    case 9:
+                        g2.drawImage(textureSprite.getSprite(8, 3, animations.getCycleProgress(), 4), (32 * j), (32 * i), this);
+                    }
+                    case 9 -> {
                         //Faire les directions avec Lucas
                         g2.drawImage(texturesImages.getImageFromTAB(0), (32 * j), (32 * i), this);
-                        g2.drawImage(textureSprite.getSprite(9,1,animations.getCycleProgress(),3), (32 * j), (32 * i), this);
-                        break;
-                    default:
-                        g2.drawImage(texturesImages.getImageFromMap(game.getMap()[i][j]), (32 * j), (32 * i), this);
-                        break;
+                        g2.drawImage(textureSprite.getSprite(9, 1, animations.getCycleProgress(), 3), (32 * j), (32 * i), this);
+                    }
+                    default ->
+                            g2.drawImage(texturesImages.getImageFromMap(game.getMap()[i][j]), (32 * j), (32 * i), this);
                 }
 
 //                if(game.getMap()[i][j] == 3 || game.getMap()[i][j] == 6 || game.getMap()[i][j] == 7 || game.getMap()[i][j] == 8 || game.getMap()[i][j] == 9){
@@ -96,9 +93,5 @@ public class GameUI extends JPanel implements Observateur {
         jFrame.addKeyListener(kl);
     }
 
-    private Image startAnimations(int posX, int posY){
-
-        return texturesImages.getImageFromTAB(0);
-    }
 
 }
