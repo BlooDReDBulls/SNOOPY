34997bb0d664fbed37fdff5fd812ec193674a2e0
TheGenyus <lucas.lalida@gmail.com>
2022-11-07 18:12:21 +0100

    Player speed = ball speed

--- Version_1/src/GamePkg/Player.java
+++ Version_1/src/GamePkg/Player.java
@@ -167,7 +167,7 @@ public class Player extends Entity{
                 public void run() {
                     unableMovement = true;
                 }
-            }, 300);
+            }, 250);
         }
     }
     @Override
